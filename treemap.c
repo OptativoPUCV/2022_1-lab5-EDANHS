101c015ff61efd8a0c770762b2f2ce4d41f602a8
Your Name <you@example.com>
2022-04-24 17:55:39 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -159,7 +159,11 @@ Pair * nextTreeMap(TreeMap * tree) {
         while(aux != NULL && tree->lower_than(aux->parent->pair->key,tree->current->pair->key) != 1){
             aux = aux->parent;
         }
-        
+        if(is_equal(tree,aux->pair->key,tree->current->pair->key)!=1){
+            tree->current = aux;
+            printf("key: %d\n",*(int*)aux->pair->key);
+            return aux->pair;
+        }
     }
     printf("NULL\n");
     return aux->pair;
