e8fd666ee12a518cd8f12958229fcff40240c03d
Your Name <you@example.com>
2022-04-24 19:21:24 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -161,7 +161,7 @@ Pair * nextTreeMap(TreeMap * tree) {
         }
         printf("key1: %d\n",*(int*)aux->pair->key);
         printf("key2: %d\n",*(int*)tree->current->pair->key);
-        if(is_equal(tree,aux->pair->key,tree->current->pair->key)==1){
+        if(is_equal(tree,aux->parent->pair->key,tree->current->pair->key)==0){
             tree->current = aux;
             printf("\n");
             printf("key1: %d\n",*(int*)aux->pair->key);
