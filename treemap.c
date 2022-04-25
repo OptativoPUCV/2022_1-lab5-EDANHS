57d400db786867e5a5fadd2e843b5834cd6c51f8
Your Name <you@example.com>
2022-04-24 20:14:32 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -153,23 +153,15 @@ Pair * nextTreeMap(TreeMap * tree) {
         tree->current = aux;
         //printf("key1: %d\n",*(int*)aux->pair->key);
         return aux->pair;
-    }else{
-        printf("Entrando en ciclo:\n");
-        aux = tree->current;
-        printf("key1: %d\n",*(int*)aux->pair->key);
-        while(aux->parent!=NULL && tree->lower_than(aux->parent->pair->key,tree->current->pair->key)!=1){
-            printf("keyCiclo: %d\n",*(int*)aux->pair->key);
-            aux = aux->parent;
-        }
-        printf("Fuera dciclo\n");
-        if(is_equal(tree,aux->pair->key,tree->current->pair->key)!=1){
+    }
+    aux = tree->current;
+    while(aux->parent!=NULL){
+        if(tree->lower_than(aux->pair->key,aux->parent->pair->key)==1){
+            aux=aux->parent;
             tree->current = aux;
-            //printf("\n");
-            printf("keyCondicion: %d\n",*(int*)aux->pair->key);
-            //printf("key2: %d\n",*(int*)tree->current->pair->key);
             return aux->pair;
         }
+        aux = aux->parent;
     }
-    printf("NULL\n");
     return NULL;
 }
