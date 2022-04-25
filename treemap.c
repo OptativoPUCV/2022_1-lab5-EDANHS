b730838504e960ca9f2d7ac869df15e6b44483cd
Your Name <you@example.com>
2022-04-24 21:25:27 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -152,7 +152,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
             }
             aux = aux->right;
         } else{
-            return aux->pair;
+            return ub_node->pair;
         }
     }
     return NULL;
